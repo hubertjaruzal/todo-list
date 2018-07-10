bae19a6b8d2500afbea7d969283e4180f3f0ce2c
hubertjaruzal <hubertjaruzal@gmail.com>
2018-07-10 18:17:57 +0200

    move fontawesome imports to the main js file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import fontawesome from '@fortawesome/fontawesome'
+import fontawesomeFAS from '@fortawesome/fontawesome-free-solid';
+import fontawesomeFAR from '@fortawesome/fontawesome-free-regular';
 
 import store, { history } from './redux/store';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.scss';
+
+fontawesome.library.add(fontawesomeFAS, fontawesomeFAR);
 
 render(
     <Provider store={store}>
