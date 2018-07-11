9178957bdbf52ac3472cff6ef5a1bb8a7893725d
hubertjaruzal <hubertjaruzal@gmail.com>
2018-07-11 09:55:12 +0200

    add generateID function

--- src/redux/actions/todo/index.js
+++ src/redux/actions/todo/index.js
@@ -1,8 +1,15 @@
+function generateID(list) {
+    if(list.length) {
+        return list[list.length - 1].id + 1
+    }
+    return 1;
+}
+
 export function addItem(data, history) {
     return (dispatch, getState) => {
         dispatch({
             type: 'ADD_ITEM',
-            data: { id: (getState().todo.list.length + 1), message: data }
+            data: { id: generateID(getState().todo.list), message: data }
         });
         history.push('/');
     }
